80aca24a57332b8059500052537f093fa179a96c
Aditya Sapate <aditya@incubyte.co>
2022-06-27 19:59:31 +0530

    Filter

---
+++ reactor-lite/src/main/java/victor/training/reactive/reactor/lite/Part14Filter.java
@@ -0,0 +1,24 @@
+package victor.training.reactive.reactor.lite;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import victor.training.reactive.reactor.lite.domain.User;
+
+public class Part14Filter {
+
+  // TODO Filter the user JESSE
+  public Mono<User> filterOne(Mono<User> mono) {
+    return null;
+  }
+
+  // TODO take the last user
+  public Flux<User> takeLast(Flux<User> flux) {
+    return null;
+  }
+
+  // TODO skip the first user
+  public Flux<User> skipFirst(Flux<User> flux) {
+    return null;
+  }
+
+}
